accadb66237ea2421c6712a3387990ce13701fdf
Rodrigo M. Mariano <rodmariano13@gmail.com>
2016-09-21 19:31:01 -0300

    Criado testes para palavra simples toda minúscula e primeira maiúscula

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class CamelCase {
 	
 	public static List<String> converterCamelCase(String original){
+		List<String> listaPalavras = new ArrayList<String>();
 		
+		listaPalavras.add(original.toLowerCase());		
 		
-		return null;		
+		return listaPalavras;		
 	}
 
 }
--- test/Tests.java
+++ test/Tests.java
@@ -1,12 +1,25 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class Tests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void palavraSimplesTodaMinuscula() {		
+		List<String> listaPalavras = CamelCase.converterCamelCase("nome");
+		
+		assertEquals(1, listaPalavras.size());		
+		assertEquals("nome", listaPalavras.get(0));
+	}
+	
+	@Test
+	public void palavraSimplesPrimeiraMaiuscula() {		
+		List<String> listaPalavras = CamelCase.converterCamelCase("Nome");
+		
+		assertEquals(1, listaPalavras.size());		
+		assertEquals("nome", listaPalavras.get(0));
 	}
 
 }
