f74f26e8baf171cc5b82fd12787489f99f715c36
Rodrigo M. Mariano <rodmariano13@gmail.com>
2016-09-21 19:59:29 -0300

    Criado teste palavraCompostaSegundaMaiuscula

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -4,9 +4,24 @@ import java.util.List;
 public class CamelCase {
 	
 	public static List<String> converterCamelCase(String original){
-		List<String> listaPalavras = new ArrayList<String>();
+		List<String> listaPalavras = new ArrayList<String>();		
 		
-		listaPalavras.add(original.toLowerCase());		
+		char[] letras = original.toCharArray();
+				
+		String palavra = "";
+
+		for(int i=0; i<letras.length; i++){			
+			char letra = letras[i];
+			
+			if( Character.isUpperCase(letra) && !palavra.isEmpty()){
+				listaPalavras.add(palavra.toLowerCase());		
+				palavra = "";
+			}
+
+			palavra = palavra + letra;						
+		}	
+		
+		listaPalavras.add(palavra.toLowerCase());
 		
 		return listaPalavras;		
 	}
--- test/Tests.java
+++ test/Tests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -20,6 +21,19 @@ public class Tests {
 		
 		assertEquals(1, listaPalavras.size());		
 		assertEquals("nome", listaPalavras.get(0));
+	}
+	
+	@Test
+	public void palavraCompostaSegundaMaiuscula() {		
+		List<String> listaPalavras = CamelCase.converterCamelCase("nomeComposto");
+		
+		assertEquals(2, listaPalavras.size());	
+		
+		List<String> listaEsperada = new ArrayList<String>();
+		listaEsperada.add("nome");
+		listaEsperada.add("composto");
+		
+		assertEquals(listaEsperada, listaPalavras);
 	}
 
 }
